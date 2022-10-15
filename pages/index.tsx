0b525a86a6ed15fd449ba6b16e24c7a4ea920782
gngo2018 <temp@Temps-MBP.attlocal.net>
2022-10-14 22:31:59 -0400

    Resolved build error for home css module

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Header from '../components/Header'
 import Hobbies from '../components/Hobbies'
 import Intro from '../components/Intro'
 import Projects from '../components/Projects'
-import styles from '../styles/home.module.css'
+import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
