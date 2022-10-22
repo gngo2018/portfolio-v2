82ff17a5ec4dca70483e0de65b01933546ba2fbd
George <gngo2018@gmail.com>
2022-10-22 10:33:05 -0400

    Added filler cards and used css grid for about me page

--- components/Hobbies/index.tsx
+++ components/Hobbies/index.tsx
@@ -1,7 +1,26 @@
-export default function Hobbies(){
-    return(
+import hobbiesStyles from './hobbies.module.css'
+
+export default function Hobbies() {
+    const hobbies = [
+        { id: 1, name: 'Tennis' },
+        { id: 2, name: 'Golf' },
+        { id: 3, name: 'Coding' },
+        { id: 4, name: 'Music' },
+        { id: 5, name: 'Outoodors' },
+    ]
+
+    return (
         <section>
             <h2>Hobbies!</h2>
+            <div className={hobbiesStyles.grid}>
+                {hobbies.map((h) => {
+                    return (
+                        <div key={h.id} className={hobbiesStyles.card}>
+                            {h.name}
+                        </div>
+                    )
+                })}
+            </div>
         </section>
     )
 }
