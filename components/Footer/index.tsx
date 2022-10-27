048970969b51449e3209d7b42661bd4dccb611d4
gngo2018 <temp@Temps-MBP.attlocal.net>
2022-10-26 23:00:32 -0400

    Added intro page content

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,30 @@
+import Image from 'next/image'
+import footerStyles from './footer.module.css'
+
 export default function Footer() {
     return(
-        <footer>
-            <h2>Desktop Footer!</h2>
+        <footer className={footerStyles.container}>
+            <div className={footerStyles.icon}>
+                <Image
+                    src='/assets/square-github.svg'
+                    alt='github'
+                    layout='fill'
+                />
+            </div>
+            <div className={footerStyles.icon}>
+                <Image
+                    src='/assets/linkedin.svg'
+                    alt='linkedin'
+                    layout='fill'
+                />
+            </div>
+            <div className={footerStyles.icon}>
+                <Image
+                    src='/assets/id-card-solid.svg'
+                    alt='resume'
+                    layout='fill'
+                />
+            </div>
         </footer>
     )
 }
--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,9 +1,25 @@
 import projectStyles from './projects.module.css'
 
 export default function Projects(){
-    return(
+    const hobbies = [
+        { id: 1, name: 'Stringify' },
+        { id: 2, name: 'G3 Sports' },
+        { id: 3, name: 'Codepen Creature' },
+        { id: 4, name: 'Static Storefront' },
+        { id: 5, name: 'Outoodors' },
+    ]
+
+    return (
         <section>
-            <h2>Projects</h2>
+            <div className={projectStyles.grid}>
+                {hobbies.map((p) => {
+                    return (
+                        <div key={p.id} className={projectStyles.card}>
+                            {p.name}
+                        </div>
+                    )
+                })}
+            </div>
         </section>
     )
 }
