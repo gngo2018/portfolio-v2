57275cb10d70a90c6fd7d9874b9d153746cc4f57
George Go <ggo@rezpower.com>
2022-10-31 15:34:48 -0400

    Adjusted server js to mimic ace

--- server.js
+++ server.js
@@ -1,29 +1,36 @@
-  // server.js
-  const { createServer } = require('http')
-  const { parse } = require('url')
-  const next = require('next')
-  
-  const dev = process.env.NODE_ENV !== 'production'
-  const port = process.env.PORT || 3000;
-  const app = next({ dev })
-  const handle = app.getRequestHandler()
-  
-  app.prepare().then(() => {
-    createServer((req, res) => {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true)
-      const { pathname, query } = parsedUrl
-  
-      if (pathname === '/a') {
-        app.render(req, res, '/a', query)
-      } else if (pathname === '/b') {
-        app.render(req, res, '/b', query)
-      } else {
-        handle(req, res, parsedUrl)
-      }
-    }).listen(port, (err) => {
-      if (err) throw err
-      console.log(`> Ready on http://localhost:${port} using server js file`)
-    })
+// server.js
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+const port = process.env.PORT || 3000
+const nextConfig = require('./next.config')
+
+const app = next({
+  dir: __dirname,
+  conf: nextConfig
+})
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query)
+    } else if (pathname === '/b') {
+      app.render(req, res, '/b', query)
+    } else {
+      handle(req, res, parsedUrl)
+    }
+  }).listen(port, (err) => {
+    if (err) {
+      throw err
+    }
+    else {
+      console.log(`> ${new Date()} Ready on http://localhost:${port} Using the fancy server js file!`);
+    }
   })
+})
