10721a48d4bf35bbe6813c85a6d899cbb6611b81
George Go <ggo@rezpower.com>
2022-11-01 11:54:10 -0400

    Fixed server js file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
+const environment = process.env.NODE_ENVIRONMENT_VARIABLE;
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
 const port = 3000
@@ -11,27 +12,26 @@ const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true)
-      const { pathname, query } = parsedUrl
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
 
-      if (pathname === '/a') {
-        await app.render(req, res, '/a', query)
-      } else if (pathname === '/b') {
-        await app.render(req, res, '/b', query)
-      } else {
-        await handle(req, res, parsedUrl)
-      }
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query)
+    } else if (pathname === '/b') {
+      app.render(req, res, '/b', query)
+    } else {
+      handle(req, res, parsedUrl)
     }
   }).listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://${hostname}:${port} using server js file!`)
+    if (err) {
+      throw err
+    }
+    else {
+      console.log(`> ${new Date()} Ready on ${hostname}:${port} Using the fancy server js file! Environment: ${environment}`);
+    }
   })
+
 })
